6f4e14a701d1d99d84170275bd04896903823a52
José Pablo Alejandro Díaz Jofré <joseapbloadiazjofre@gmail.com>
2021-11-15 04:43:08 -0300

    se prepara la database, se agregan templates y urls a la carpeta main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 # Create your views here.
+def home(response):
+    return render(response,"main/home.html",{})
