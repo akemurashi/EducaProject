2130900beb790fa355f19dad7ddd25b7bf11470e
José Pablo Alejandro Díaz Jofré <joseapbloadiazjofre@gmail.com>
2021-12-04 14:24:09 -0300

    listos

--- Educapro/settings.py
+++ Educapro/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
       os.path.join(BASE_DIR, 'static'),
 )
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
@@ -140,3 +140,7 @@ CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_REDIRECT_URL = "/ramos"
 LOGOUT_REDIRECT_URL = "/"
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
