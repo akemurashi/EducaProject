d5865628ff4c1776580658ed9016ca65440180a9
José Pablo Alejandro Díaz Jofré <joseapbloadiazjofre@gmail.com>
2021-12-05 15:33:18 -0300

    pruebas3

--- Educapro/settings.py
+++ Educapro/settings.py
@@ -128,14 +128,14 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
       os.path.join(BASE_DIR, 'static'),
       os.path.join(BASE_DIR, 'media'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
